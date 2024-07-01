ffde4b65aa3281d4e794760dcddfee0490ed205f
Thandeka Ncube <96820021+Thandeka132@users.noreply.github.com>
2024-07-01 21:49:44 +0200

    Initial commit

---
+++ WasteManagementApplication/src/test/java/com/enviro/assessment/grad001/thandekancube/WasteManagementApplication/WasteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.thandekancube.WasteManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
